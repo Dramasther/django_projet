daf75dc252340531fc82c760441e00d2bb4392ea
DRAMANE DIALLO <dramane1diallo@gmail.com>
2024-05-05 23:34:05 +0000

    feat: add comment and email sending

--- first/first/settings.py
+++ first/first/settings.py
@@ -101,6 +101,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "dramane1diallo@gmail.com"
+EMAIL_HOST_PASSWORD = "egkxnyxlilkvfnio"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
--- first/secondapp/views.py
+++ first/secondapp/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .models import Post
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 from django.views.generic import ListView
 
 class PostListView(ListView):
@@ -46,3 +47,24 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+            f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+            f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'dramane1diallo@gmail.com',[cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'secondapp/post/share.html', {'post': post,'form': form,'sent': sent})
