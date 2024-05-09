97ed027b14a7da3b37acb95cc47da008376077ea
DRAMANE DIALLO <dramane1diallo@gmail.com>
2024-05-09 12:53:01 +0000

    feat: install django_taggit and add tags in comment

--- first/secondapp/models.py
+++ first/secondapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+from taggit.managers import TaggableManager
 from django.contrib.auth.models import User
 
 class PublishedManager(models.Manager):
@@ -26,7 +27,8 @@ class Post(models.Model):
         updated = models.DateTimeField(auto_now=True)
         status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
         objects = models.Manager() # The default manager.
-        published = PublishedManager() # Our custom manager.
+        published = PublishedManager() # Our custom manager
+        tags = TaggableManager()
         def get_absolute_url(self):
             return reverse('secondapp:post_detail',
                 args=[self.publish.year,
