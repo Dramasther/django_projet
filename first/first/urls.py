27d5ed86eccdb9ddf35ce8ab4bdcaa82b61fa9e8
DRAMANE DIALLO <dramane1diallo@gmail.com>
2024-05-24 00:19:59 +0000

    feat: add sitemap

--- first/first/urls.py
+++ first/first/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.sitemaps.views import sitemap
+from secondapp.sitemaps import PostSitemap
+
+sitemaps = {
+ 'posts': PostSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('secondapp/',include('secondapp.urls',namespace='secondapp')),
+    path('secondapp/',include('secondapp.urls',namespace='secondapp')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},name='django.contrib.sitemaps.views.sitemap')
 ]
