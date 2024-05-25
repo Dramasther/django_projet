22e99d19836cfecb501c499416cdbda650ced9f2
DRAMANE DIALLO <dramane1diallo@gmail.com>
2024-05-25 15:22:01 +0000

    feat: add field search

--- first/secondapp/urls.py
+++ first/secondapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import post_detail,post_list,post_share
+from .views import post_detail,post_list,post_share,post_search
 from .feeds import LatestPostsFeed
 
 app_name='secondapp'
@@ -11,4 +11,5 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',post_detail,name='post_detail'),
     path('<int:post_id>/share/',post_share, name='post_share'),
     path('feed/', LatestPostsFeed(), name='post_feed'),
+    path('search/', post_search, name='post_search'),
 ]
